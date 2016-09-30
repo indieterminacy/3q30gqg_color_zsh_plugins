cd92693d9e70a76565dff61600de53f7e44e5eb0
indieterminacy <indieterminacy@nortonspeal.com>
2016-09-30 20:03:57 +0200

    Random commit

---
+++ oqo_colors.zsh
@@ -0,0 +1,15 @@
+# # fancy list-colors originally by nicoulaj
+#https://github.com/hchbaw/list-colors.zsh
+# Not fully working
+# -- >8 --
+# fpath+=~/.zsh
+# autoload -Uz list-colors-init; list-colors-init
+# zstyle -e ':completion:*:default' list-colors list-colors list-colors-aux
+# zplug "hchbaw/list-colors.zsh"
+
+# Show color pallet for zsh/bash on 16 and 256 color terminal
+#https://github.com/yonchu/shell-color-pallet
+# Make shell's echo colorful with easier method.
+# In sh, bash, zsh, ksh and
+# fish.
+# https://github.com/PeterDaveHello/ColorEchoForShell
