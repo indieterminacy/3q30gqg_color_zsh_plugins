4e1dd812deeb1481fcf6704b203370c1e56ea754
Indieterminacy <indieterminacy@nortonspeal.com>
2018-12-03 15:01:46 +0100

    Added Tmux specific color plugin configuration

---
+++ oq_jq_tmux_configuration.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/zsh
+
+export TERM=xterm-256color
+
+# export TERM=screen-256color
+# If you have problems with tmux not clearing the background colors correctly 
+#when using the screen term setting, you can try:
+# export TERM=screen-256color-bce
